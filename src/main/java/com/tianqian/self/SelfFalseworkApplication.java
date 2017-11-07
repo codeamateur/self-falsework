4024b817a791b702c3c8981571d46e0bbf163efe
张金星 <zhangjinxing@suzhutec.com>
2017-11-07 20:37:55 +0800

    11

--- src/main/java/com/tianqian/self/SelfFalseworkApplication.java
+++ src/main/java/com/tianqian/self/SelfFalseworkApplication.java
@@ -1,9 +1,11 @@
 package com.tianqian.self;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.tianqian.self.dao")
 public class SelfFalseworkApplication {
 
 	public static void main(String[] args) {
