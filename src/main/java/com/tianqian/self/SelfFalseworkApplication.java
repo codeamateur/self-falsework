bccfc99ffb5a6a7b0345e357a52ce22d0081c814
张金星 <zhangjinxing@suzhutec.com>
2017-11-21 10:37:46 +0800

    war deploy

--- src/main/java/com/tianqian/self/SelfFalseworkApplication.java
+++ src/main/java/com/tianqian/self/SelfFalseworkApplication.java
@@ -3,6 +3,8 @@ package com.tianqian.self;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,7 +12,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @MapperScan(basePackages = "com.tianqian.self.dao")
 @EnableScheduling
 @EnableWebMvc
-public class SelfFalseworkApplication {
+public class SelfFalseworkApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SelfFalseworkApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SelfFalseworkApplication.class, args);
 	}
